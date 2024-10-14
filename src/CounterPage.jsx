9faaf71f0cf1ca0a2efb4930971fd781b3eaa5e9
stoffe-fe23 <kristoffer.bengtsson@gritacademy.se>
2024-10-14 16:14:27 +0200

    felkoll

--- src/CounterPage.jsx
+++ src/CounterPage.jsx
@@ -5,6 +5,10 @@ export default function CounterPage() {
     const buttons = [-9, -8, -7, -6, -5, -4, -3, -2, -1, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
     const [counterValue, counterValueDispatch] = useReducer((state, action) => {
+
+        if (!buttons.includes(Number(action)))
+            return state;
+
         return Number(state) + Number(action);
     }, 0);
 
